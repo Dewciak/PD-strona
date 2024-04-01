d067f3efc5ed61b1941fd35f782d7cb2a7930c48
wiktor Laptop <wiktorskid@wp.pl>
2024-04-01 22:48:38 +0200

    deploy

--- PD-fotografia/vite.config.js
+++ PD-fotografia/vite.config.js
@@ -4,10 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ["bower_components/aos/dist/aos.js"],
-    },
+  base: "/PD-strona/"
   },
 });
 

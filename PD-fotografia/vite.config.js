67afbcb6a5a93cd95c5ced0c0ce214fbf30e731d
Dewciak <wiktorskid@wp.pl>
2024-04-08 17:24:11 +0200

    moze tera 06.04

--- PD-fotografia/vite.config.js
+++ PD-fotografia/vite.config.js
@@ -1,12 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/PD-FOTOGRAFIA/",
   plugins: [react()],
-  scripts: {
-    build: "vite build",
-    preview: "vite preview",
-  },
-  base: "https://dewciak.github.io/PD-strona/",
 });

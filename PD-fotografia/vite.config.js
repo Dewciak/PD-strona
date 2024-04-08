a678cab43dc5f5346237f2aa343f42936ae84964
Dewciak <wiktorskid@wp.pl>
2024-04-08 17:46:51 +0200

    moze tera#2

--- PD-fotografia/vite.config.js
+++ PD-fotografia/vite.config.js
@@ -4,5 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/PD-FOTOGRAFIA/",
+  cssCodeSplit: false,
   plugins: [react()],
 });

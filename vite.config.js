81f8fc478541208930b23d690b04fdf296af8618
Witek <wiktorskid@wp.pl>
2024-04-10 21:17:21 +0200

    fdg

--- vite.config.js
+++ vite.config.js
@@ -5,21 +5,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   base: "/PD-fotografia/",
   plugins: [react()],
-<<<<<<< HEAD
-});
-
-// export default {
-//   build: {
-//     rollupOptions: {
-//       external: ['bower_components/aos/dist/aos.js']
-//     }
-//   }
-// }
-=======
   scripts: {
     build: "vite build",
     preview: "vite preview",
   },
   base: "https://dewciak.github.io/PD-strona/",
 });
->>>>>>> 9a054b6 (próba)
